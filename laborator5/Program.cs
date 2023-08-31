79804c05d5b0b14211cbb4a421eefd6f3b7c5f9b
Andrei Corovei <andrei.corovei@qubiz.com>
2023-08-31 16:41:04 +0300

    Add project files.

---
+++ laborator5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace laborator5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Scrieti o functie care va calcula numarul de caractere goale dintr-un string
+            */
+            //string sir = string.Empty;
+            Console.WriteLine(" introduceti un sir de caractere:");
+            string sir = Console.ReadLine();
+            Console.WriteLine(" nr de spatii este :" + GetWhiteSpacesNo(sir));
+       }
+        static int GetWhiteSpacesNo(string sir)
+        {
+            if (string.IsNullOrWhiteSpace(sir) )
+            {
+                return 0;
+            }
+            int whiteSpaces = 0;
+            for (int i = 0; i< sir.Length; i++)
+            {
+                if (char.IsWhiteSpace(sir[i]))
+                {
+                    whiteSpaces++;
+                }
+            }
+            return whiteSpaces;
+        }
+    }
+}
---
+++ laborator5exercitii/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace laborator5exercitii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex1();
+            //Ex2();
+            //Ex3();
+            //Ex4();
+            //Ex5();
+            //Ex6();
+            //Ex7();
+        }
+
+        static void Ex1()
+        {
+            /*Scrieti un program care va afisa pozitia unui substring intr-un string, ambele
+            siruri de caractere fiind citite de la tastatura*/
+
+            Console.WriteLine("Introduceti primul sir: ");
+            string string1 = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(string1))
+            {
+                Console.WriteLine("Nu ai introdus nimic, sau ai introdus un spatiu gol.  Reincearca");
+                return;
+            }
+            Console.WriteLine("Introduceti al 2-lea sir: ");
+            string string2 = Console.ReadLine();
+
+            if(string.IsNullOrWhiteSpace(string1))
+            {
+                Console.WriteLine("Nu ai introdus nimic, sau ai introdus un spatiu gol.  Reincearca");
+                return;
+            }
+            int pozitie = string1.IndexOf(string2);
+            if (pozitie == -1)
+            {
+                Console.WriteLine($"{string2} nu se gaseste deloc in { string1}");
+            }
+            else Console.WriteLine($"{string2} incepe de pe pozitia {pozitie} in { string1}");
+        }
+
+        static void Ex2()
+            /*
+             Scrieti un program care sa verifice daca un sir de caractere citit de la 
+             tastatura este sau nu palindrom.
+             */
+    }
+}
